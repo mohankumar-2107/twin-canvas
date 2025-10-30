947e6ba4679155023534ce85ed3f83031c6e9530
Mohan Kumar GB <mohankumargb666@gmail.com>
2025-10-30 19:32:07 +0530

    Connect frontend to live Render server

--- js/draw.js
+++ js/draw.js
@@ -1,5 +1,5 @@
 document.addEventListener('DOMContentLoaded', () => {
-    const socket = io();
+    const socket = io('https://your-render-url-goes-here.onrender.com');
     const canvas = document.getElementById('canvas');
     const ctx = canvas.getContext('2d');
 
